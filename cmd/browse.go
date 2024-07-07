d2eddb6f16adfd4b0e14d494c520a6f265b41907
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-07-06 20:47:53 -0400

    Start implementiung browse

--- cmd/browse.go
+++ cmd/browse.go
@@ -2,7 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"tempvault/config"
+
+	fzf "github.com/junegunn/fzf/src"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +19,58 @@ var browseCmd = &cobra.Command{
 	Use:   "browse",
 	Short: "Browse the vault.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Browse tempvault!")
+		vaultDir, err := config.GetTempVaultDir()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		inputChan := make(chan string)
+		go func() {
+			err := filepath.Walk(vaultDir, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if !info.IsDir() {
+					inputChan <- filepath.Base(path)
+				}
+				return nil
+			})
+			if err != nil {
+				fmt.Printf("An error occurred while walking through the vault directory, error: %s\n", err)
+			}
+			close(inputChan)
+		}()
+
+		outputChan := make(chan string)
+		go func() {
+			for s := range outputChan {
+				fmt.Println("Got: " + s)
+			}
+		}()
+
+		exit := func(code int, err error) {
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+			}
+			os.Exit(code)
+		}
+
+		// Build fzf.Options
+		options, err := fzf.ParseOptions(
+			true, // whether to load defaults ($FZF_DEFAULT_OPTS_FILE and $FZF_DEFAULT_OPTS)
+			[]string{"--preview", "cat " + vaultDir + "/{}", "--multi"},
+		)
+		if err != nil {
+			exit(fzf.ExitError, err)
+		}
+
+		// Set up input and output channels
+		options.Input = inputChan
+		options.Output = outputChan
+
+		// Run fzf
+		code, err := fzf.Run(options)
+		exit(code, err)
 	},
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,19 @@ module tempvault
 go 1.22.4
 
 require (
+	github.com/charlievieth/fastwalk v1.0.4 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/junegunn/fzf v0.0.0-20240707000156-4ef1cf5b35ca // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
